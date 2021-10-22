41a8096685eb11b40f0513424c5a29da919d0b02
Alexander Shukan <avshukan@gmail.com>
2021-10-22 12:09:34 +1100

    add: postgres

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@
 import welcome from './welcome.js';
 import users from './users.js';
 import session from './session.js';
+import db from './db';
 
 const controllers = [
   welcome,
   users,
   session,
+  db,
 ];
 
 export default (app) => controllers.forEach((f) => f(app));
