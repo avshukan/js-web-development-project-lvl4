06a6fc469673dfcdf9076381e48fc1e56506f086
Alexander Shukan <avshukan@gmail.com>
2021-10-22 12:09:34 +1100

    add: postgres

---
+++ server/routes/db.js
@@ -0,0 +1,24 @@
+// @ts-check
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+export default (app) => {
+  app
+    .get('/db', async (req, res) => {
+      try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }
+    })
+};
