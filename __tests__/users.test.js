e61f830fc4e80e76546c3ef2170051d00079e8f9
Alexander Shukan <avshukan@gmail.com>
2021-11-10 12:22:33 +1100

    add: test for edit user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -30,7 +30,6 @@ describe('test users CRUD', () => {
       method: 'GET',
       url: app.reverse('users'),
     });
-
     expect(response.statusCode).toBe(200);
   });
 
@@ -39,7 +38,6 @@ describe('test users CRUD', () => {
       method: 'GET',
       url: app.reverse('newUser'),
     });
-
     expect(response.statusCode).toBe(200);
   });
 
@@ -60,6 +58,90 @@ describe('test users CRUD', () => {
     };
     const user = await models.user.query().findOne({ email: params.email });
     expect(user).toMatchObject(expected);
+
+    const existingParams = testData.users.existing;
+    const existingResponse = await app.inject({
+      method: 'POST',
+      url: app.reverse('users'),
+      payload: {
+        data: existingParams,
+      },
+    });
+    expect(existingResponse.statusCode).toBe(422);
+  });
+
+  it('edit', async () => {
+    const nonexistentParams = testData.users.nonexistent;
+    const nonexistentResponse = await app.inject({
+      method: 'GET',
+      url: app.reverse(`users/${nonexistentParams.id}/edit`),
+    });
+    expect(nonexistentResponse.statusCode).toBe(401);
+
+    const responseSignIn = await app.inject({
+      method: 'POST',
+      url: app.reverse('session'),
+      payload: {
+        data: testData.users.olddata,
+      },
+    });
+    // после успешной аутентификации получаем куки из ответа,
+    // они понадобятся для выполнения запросов на маршруты требующие
+    // предварительную аутентификацию
+    const [sessionCookie] = responseSignIn.cookies;
+    const { id, name, value } = sessionCookie;
+    const cookie = { [name]: value };
+
+    const olddataParams = testData.users.olddata;
+    const olddataResponse = await app.inject({
+      method: 'GET',
+      url: app.reverse(`users/${olddataParams.id}/edit`),
+    });
+    expect(olddataResponse.statusCode).toBe(200);
+
+    // добавить обновление данных
+    // PATCH /users/:id - обновление пользователя
+    // 302 - успешное обновление с переадресацией (куда?)
+    // 401 - ошибка обновления, потому что id не соответствует id пользователя
+    // 422 - ошибка обновления по другим причинам
+    const unauthorizedId = 1;
+    const newParams = testData.users.newdata;
+    const unauthorizedResponse = await app.inject({
+      method: 'PATCH',
+      url: app.reverse(`users/${unauthorizedId}/edit`),
+      payload: {
+        data: newParams,
+      },
+    });
+    // const unauthorizedUser = await models.user.query().findOne({ id: unauthorizedId });
+    expect(unauthorizedResponse.statusCode).toBe(401);
+    // expect(user).toBeNull();
+
+    const badParams = {};
+    const badResponse = await app.inject({
+      method: 'PATCH',
+      url: app.reverse(`users/${id}/edit`),
+      payload: {
+        data: badParams,
+      },
+    });
+    const user = await models.user.query().findOne({ id: id });
+    expect(badResponse.statusCode).toBe(422);
+    // expect(user).toBeNull();
+
+    const newResponse = await app.inject({
+      method: 'PATCH',
+      url: app.reverse(`users/${id}/edit`),
+      payload: {
+        data: newParams.email,
+      },
+    });
+    const newUser = await models.user.query().findOne({ id: id });
+    const newEmail = newUser.email;
+    const newPasswordDigest = newUser.passwordDigest;
+    expect(newResponse.statusCode).toBe(302);
+    expect(newEmail).toBe(newParams);
+    expect(newPasswordDigest).toBe(encrypt(newParams.password));
   });
 
   it('delete', async () => {
@@ -97,7 +179,7 @@ describe('test users CRUD', () => {
       },
     });
     const badUser = await models.user.query().findOne({ id: params.id });
-    expect(badResponse.statusCode).toBe(302);
+    expect(badResponse.statusCode).toBe(422);
     expect(badUser).toBeNull();
   });
 
