65beeb51108ae32b991489a189016407d7c2be89
Alexander Shukan <avshukan@gmail.com>
2021-12-06 14:45:02 +1100

    add: task entity

---
+++ server/migrations/20211206033530_add_task_entity.js
@@ -0,0 +1,12 @@
+exports.up = (knex) => knex.schema.createTable('tasks', (table) => {
+    table.increments('id').primary();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.string('name');
+    table.string('description');
+    table.integer('statusId');
+    table.integer('creatorId');
+    table.integer('executorId');
+});
+
+exports.down = (knex) => knex.schema.dropTable('tasks');
