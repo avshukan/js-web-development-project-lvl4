6f9842516985f69a72c3435077f021440b0bf634
Alexander Shukan <avshukan@gmail.com>
2021-12-10 10:53:24 +1100

    add: task controller (create new task)

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,6 +4,7 @@ import i18next from 'i18next';
 
 export default (app) => {
   app
+
     .get('/tasks', { name: 'page of tasks list' }, async (req, reply) => {
       if (!req.session.get('id')) {
         req.flash('error', i18next.t('flash.authError'));
@@ -14,6 +15,7 @@ export default (app) => {
       reply.render('tasks/list', { tasks });
       return reply;
     })
+
     .get('/tasks/new', { name: 'page to create task' }, async (req, reply) => {
       if (!req.session.get('id')) {
         req.flash('error', i18next.t('flash.authError'));
@@ -24,22 +26,49 @@ export default (app) => {
       reply.render('tasks/new', { task });
       return reply;
     })
+
     .get('/tasks/:id', { name: 'page to show task' }, async (_req, reply) => {
       reply.redirect(app.reverse('root'));
       return reply;
     })
+
     .get('/tasks/:id/edit', { name: 'page to update task' }, async (_req, reply) => {
       reply.redirect(app.reverse('root'));
       return reply;
     })
-    .post('/tasks', { name: 'create task' }, async (_req, reply) => {
-      reply.redirect(app.reverse('root'));
-      return reply;
+
+    .post('/tasks', { name: 'create task' }, async (req, reply) => {
+      const creatorId = req.session.get('id');
+      if (!creatorId) {
+        req.flash('error', i18next.t('flash.authError'));
+        reply.redirect(app.reverse('root'));
+        return reply;
+      }
+      const { data } = req.body;
+      try {
+        const status = await app.objection.models.status.query().findById(data.statusId);
+        if (!status) {
+          throw new Error();
+        }
+        const task = await app.objection.models.task.fromJson({ ...data, creatorId });
+        await app.objection.models.task.query().insert(task);
+        req.flash('success', i18next.t('flash.tasks.create.success'));
+        reply.redirect(app.reverse('page of tasks list'));
+        return reply;
+      } catch (error) {
+        console.log('error', error);
+        req.flash('error', i18next.t('flash.tasks.create.error'));
+        reply.statusCode = 422;
+        reply.render('tasks/new', { task: data, errors: error.data });
+        return reply;
+      }
     })
+
     .patch('/tasks/:id', { name: 'update task' }, async (_req, reply) => {
       reply.redirect(app.reverse('root'));
       return reply;
     })
+
     .delete('/tasks/:id', { name: 'delete task' }, async (_req, reply) => {
       reply.redirect(app.reverse('root'));
       return reply;
