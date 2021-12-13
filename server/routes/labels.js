e4c143d5af3a1d08b451ecb7d24535630f54934d
avshukan <avshukan@gmail.com>
2021-12-13 16:12:00 +1100

    fix: some errors

--- server/routes/labels.js
+++ server/routes/labels.js
@@ -1,8 +1,19 @@
+import i18next from 'i18next';
+
 // @ts-check
 export default (app) => {
   app
 
-    .get('/labels', { name: 'page of labels list' }, async (_req, reply) => reply.redirect(app.reverse('root')))
+    .get('/labels', { name: 'page of labels list' }, async (req, reply) => {
+      if (!req.session.get('id')) {
+        req.flash('error', i18next.t('flash.authError'));
+        reply.redirect(app.reverse('root'));
+        return reply;
+      }
+      const labels = await app.objection.models.label.query().orderBy('updatedAt', 'desc');
+      reply.render('labels/list', { labels });
+      return reply;
+    })
 
     .get('/labels/new', { name: 'page of create label' }, async (_req, reply) => reply.redirect(app.reverse('root')))
 
