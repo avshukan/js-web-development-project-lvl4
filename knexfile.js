3e7528ef629840a8af7f8c307fa684ef7375458c
Alexander Shukan <avshukan@gmail.com>
2021-12-23 18:05:41 +1100

    try: heroku+

--- knexfile.js
+++ knexfile.js
@@ -47,7 +47,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: process.env.DATABASE_URL,
     migrations,
   },
 };
